2aced8a8937b7aef507977b95481c8be19531c50
eldaroo <darioegea@gmail.com>
2023-02-25 12:58:41 -0300

    =add persistence on the local store, this'll possibly be usefull for users login

--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
 import { createStore, combineReducers } from 'redux';
 import { orders } from './orders/reducers';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
 
 const reducers = {
     orders,
 };
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2
+}
 const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-export const configureStore = () => createStore(rootReducer);
+export const configureStore = () => createStore(persistedReducer);
